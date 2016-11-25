cc9b5f3b875f9f7a10f9335717176d46c7fbaa07
bakeKitunE <r.toku357951@gmail.com>
2016-11-25 17:18:39 +0900

    Answer

---
+++ 2016-11-25/Answer/bakeKitunE.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Test
+{
+    public static void Main()
+    {
+        var a = Console.ReadLine().Split(' ');
+        var b = Console.ReadLine().Split(' ');
+        Console.WriteLine("{0} {1}\n{2} {3}", b[1], b[0], a[1], a[0]);
+    }
+}
