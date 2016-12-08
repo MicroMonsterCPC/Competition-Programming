10d62a0892ca3c48a5c852347cc14e99aae98301
bakeKitunE <r.toku357951@gmail.com>
2016-12-08 16:48:56 +0900

    よけいな１行を削除

--- 2016-12-08/Answer/bakeKitunE.cs
+++ 2016-12-08/Answer/bakeKitunE.cs
@@ -7,9 +7,8 @@ namespace program
         static void Main(string[] args)
         {
             string[] input = Console.ReadLine().Split('_');
-            for (int i = 0; i < input.Length; i++)
+            for (int i = 1; i < input.Length; i++)
             {
-                if (i == 0) continue;
                 input[i] = input[i][0].ToString().ToUpper() + input[i].Substring(1);
             }
             Console.WriteLine(string.Join("", input));
